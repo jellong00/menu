de6914adf048a84da1e0e2e77c0da715d83d93f5
jellong00 <tjddms2871@snu.ac.kr>
2026-07-20 19:47:28 +0900

    Update menu.py

--- menu.py
+++ menu.py
@@ -12,10 +12,8 @@ if st.button("메뉴 추천받기 🍲", use_container_width=True):
     st.session_state.revealed = True
 
 if st.session_state.revealed:
-    # 👇 여기에 본인이 준비한 이미지 URL 넣기
-    image_url = "https://search.pstatic.net/sunny/?src=https%3A%2F%2Fupload3.inven.co.kr%2Fupload%2F2023%2F09%2F12%2Fbbs%2Fi16370258321.jpg%3FMW%3D800&type=sc960_832"
 
-    st.image(image_url, use_container_width=True)
+    st.image("KakaoTalk_20260720_194437742.jpg", use_container_width=True)
     st.markdown("<h2 style='text-align:center;'>메뉴는 무슨 메뉴야 ㅋㅋㅋ</h2>", unsafe_allow_html=True)
 
     if st.button("다시하기"):
